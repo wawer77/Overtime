6e4d74e74e49b1e2d7069e6b111eb24299b6cf0d
wawer77 <tomek.06@wp.pl>
2018-08-30 06:58:15 -0400

    Implement post creation process and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,36 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :destroy]
+  
   def index
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post, notice: "Your post was created successfully"
+    else
+      render :new
+    end
+  end
+  
+  def show
+  end
+  
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,4 +14,22 @@ describe "navigate" do
       expect(page).to have_content(/Posts/)
     end
   end
+  
+  describe "creation" do
+    before do
+      visit new_post_path
+    end
+    
+    it "has a new form that can be reached" do
+      expect(page.status_code).to eq(200)
+    end
+    
+    it 'allows users to create a new post from the /new page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Whatever"
+      click_on "Save"
+      expect(page).to have_content("Whatever")
+    end
+  end
+    
 end
