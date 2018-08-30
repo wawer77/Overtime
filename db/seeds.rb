723c4e87ef8adfd84840edd6897001baef308649
wawer77 <tomek.06@wp.pl>
2018-08-30 06:17:38 -0400

    Create seed file with default post content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+end
+
+puts "100 Posts have been created"
