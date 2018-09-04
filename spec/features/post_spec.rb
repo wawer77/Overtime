2bbc9a5f2eef6cc53fbad16c647fd7cfe490dd75
wawer77 <tomek.06@wp.pl>
2018-09-04 12:33:03 -0400

    Add new post button to navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -29,6 +29,15 @@ describe "navigate" do
       
   end
   
+  describe 'new' do
+    
+    it 'has a link from homepage' do 
+      visit root_path  
+      click_link("new_post_from_nav") 
+      expect(page.status_code).to eq(200)
+    end
+  end
+  
   describe "creation" do
     before do
       visit new_post_path
