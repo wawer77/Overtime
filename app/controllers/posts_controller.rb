2ca014b3aabcccad9ae465aaaa5bc639d45ad7a9
wawer77 <tomek.06@wp.pl>
2018-09-04 12:25:56 -0400

    Add edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.all
@@ -16,13 +16,23 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: "Your post was created successfully"
     else
-      render :new
+      render new
     end
   end
   
   def show
   end
   
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+    redirect_to @post, notice: "Your post was edited successfully"
+    else
+      render edit
+    end
+  end
   
   private
   
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -48,9 +48,29 @@ describe "navigate" do
     it "will have user associated it" do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
-      click_on "Save"
+      click_on 'Save'
       expect(User.last.posts.last.rationale).to eq("User Association")
     end
+  end
   
+  describe "edit" do
+    before do
+      @post = FactoryBot.create(:post)
+    end
+    
+    it "can be reached by clicking on index page" do
+      visit posts_path
+      click_link("edit_#{@post.id}") 
+      expect(page.status_code).to eq(200)
+    end
+     
+    it "can be edited" do
+      visit edit_post_path(@post)
+      expect(page.status_code).to eq(200)
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on "Save"
+      expect(page).to have_content("Edited content")
+    end
   end
 end
