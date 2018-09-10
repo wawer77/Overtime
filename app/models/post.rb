e8ebc6ba3d507c3423d78dd650994d0ea24d2cfa
wawer77 <tomek.06@wp.pl>
2018-09-10 08:32:53 -0400

    Implement custom scope for Post by users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   #Made optional to pass test spec/models/post - when creating post a user must be defined 
   validates_presence_of :date, :rationale
   enum status: { submitted: 0, approved: 1, rejected: 2 }
+  
+  scope :posts_by, ->(user) { where(user_id: user.id) }
 end
 
