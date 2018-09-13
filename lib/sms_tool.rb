5d25060ebd6a7e6e0372b4eb742e25281810afe9
wawer77 <tomek.06@wp.pl>
2018-09-13 07:27:16 -0400

    Integrate live connection with Twilio for SMS notifications

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,20 @@
 module SmsTool
+# establish the API credentials
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+# send the message via the client
   def self.send_sms(number:, message:)
-    puts "Sending SMS..."
-    puts "#{message} to #{number}"
+    @client.messages.create(
+      from:  ENV['TWILIO_PHONE_NUMBER'],
+      to: "+48#{number}",
+      body: "#{message}"
+    )
+        puts "Sending SMS..."
+        puts "#{message} to #{number}"
   end
 end
+
