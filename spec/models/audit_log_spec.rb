977d0979fc1444788a92b1170d4c331c729e9e03
wawer77 <tomek.06@wp.pl>
2018-10-04 09:51:17 -0400

    Integrate validations and defaults for audit logs attributes

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 RSpec.describe AuditLog, type: :model do
 
   
-  describe 'creation'
-  before do
-    @user = FactoryBot.create(:user)
-    @audit_log = FactoryBot.create(:audit_log, user_id: @user.id)
-  end
-  
-  it 'can be created' do
-      expect(@audit_log).to be_valid
+  describe 'creation' do
+    before do
+      @user = FactoryBot.create(:user)
+      @audit_log = FactoryBot.create(:audit_log, user_id: @user.id)
+    end
+    
+    it 'can be created' do
+        expect(@audit_log).to be_valid
     end
   end
-
   
   describe 'validations' do
       before do
@@ -22,24 +21,23 @@ RSpec.describe AuditLog, type: :model do
       end
 
       it 'should be required to have user association' do
-      @audit_log.user_id = nil  
-      expect(@audit_log).to_not be_valid
+        @audit_log.user_id = nil  
+        expect(@audit_log).to_not be_valid
       end
   
       it 'should alwyas have a status' do
-      @audit_log.status = nil    
-      expect(@audit_log).to_not be_valid
+        @audit_log.status = nil    
+        expect(@audit_log).to_not be_valid
       end
   
       it 'should be required to have a start_date' do
-      @audit_log.start_date= nil    
-      expect(@audit_log).to_not be_valid
+        @audit_log.start_date= nil    
+        expect(@audit_log).to_not be_valid
       end    
     
-      xit 'should have start date equal to 6days prior' do
-      expect(@audit_log).to_not be_valid
+      it 'should have start date equal to 6days prior' do
+        new_audit_log = FactoryBot.create(:audit_log, user_id: @user.id)
+        expect(new_audit_log.start_date).to eq(Date.today - 6.days)
       end    
-    
-  6:11  
-  end 
-
+  end
+end
